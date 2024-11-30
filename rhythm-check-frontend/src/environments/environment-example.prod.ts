69c14bdc31c3f833a80bf8c0fc8ed0bdeb028be4
AngelGarciaAguilera <angelgarciaaguilera2004@gmail.com>
2024-11-30 15:12:03 +0100

    feat: initial commit, projects initializated

---
+++ rhythm-check-frontend/src/environments/environment-example.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  spotify: {
+    apiUrl: 'https://api.spotify.com/v1/',
+    clientId: 'your-client-id-production',
+    clientSecret: 'your-client-secret-production',
+  },
+};
---
+++ rhythm-check-frontend/src/environments/environment-example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  spotify: {
+    apiUrl: 'https://api.spotify.com/v1/',
+    clientId: 'your-client-id-local',
+    clientSecret: 'your-client-secret-local',
+  },
+};
